9389ae202b32fddd563a6fc1dcfc3d4afb0da175
zyx <1272686158@qq>
2021-11-12 22:35:35 +0800

    The part of vision can work properly now

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import vision
+import time
 from multiprocessing import Process, Queue
 
+from yolov5.utils.general import LOGGER
+
 def main():
-    q = Queue()
-    visionProc = Process(target = vision.videoCap, args=(q,))
-    #testProc = Process(target = read, args=(q,))
+    q1 = Queue() # 视觉模块发给虚拟形象模块的信号队列
+    visionProc = Process(target = vision.videoCap, args=(q1,))
+    testProc = Process(target = read, args=(q1,))
     visionProc.start()
-    #testProc.start()
+    testProc.start()
     visionProc.join()
-    #testProc.terminate()
+    testProc.terminate()
     pass
 
 def read(q):
     while True:
         value = q.get(True)
         print('Get %s from queue' % value)
+        time.sleep(0.1)
+
 
 if __name__ == '__main__':
     main()
--- vision/__init__.py
+++ vision/__init__.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+from multiprocessing import Queue
+from yolov5.detect import run
 
 def videoCap(q):
     cap = cv.VideoCapture(0)
@@ -8,10 +10,14 @@ def videoCap(q):
             print("Do not find your camera.")
             break
         cv.imshow("frame", frame)
-        if True:
-            q.put('DETECTED')
-        if cv.waitKey(50) & 0xff == ord('q'):
+        cv.imwrite('./yolov5/data/images/src1.jpg', frame)
+        flag = run()
+        if flag and q.empty():
+            # q.put('DETECTED')
+            print("DETECTED!")
+        if cv.waitKey(1000) & 0xff == ord('q'):
             break
 
 if __name__ == '__main__':
-    videoCap()
+    q = Queue()
+    videoCap(q)
